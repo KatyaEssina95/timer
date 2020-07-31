07363ba1ffcae35e0bf36827de040572ba32cdf1
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-07-31 20:46:55 +0100

    basic timer app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Hello Katya
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Stopwatch from "./stopwatch/components/Stopwatch";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Stopwatch />
+      </div>
+    );
+  }
 }
-
 export default App;
